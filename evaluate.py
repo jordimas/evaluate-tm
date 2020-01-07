1392bccbc1a29b8ebf18ca129c41ed404cf3a2a6
Jordi Mas <jmas@softcatala.org>
2020-01-07 19:51:42 +0100

    NIST

--- evaluate.py
+++ evaluate.py
@@ -20,6 +20,7 @@
 
 import nltk
 import warnings
+from nltk.translate import nist_score, bleu_score
 warnings.filterwarnings("ignore")
 
 def show_bleu(reference_file, hypotesis_file):
@@ -43,19 +44,35 @@ def show_bleu(reference_file, hypotesis_file):
         for i in range(0, len(lines_hyp)):
             strings_hyp.append(lines_hyp[i].split())
 
-        scored = 0
-        for i in range(0, len(strings_ref)):
-#            print("Ref->" + str(strings_ref[i]))
-#            print("Hyp->" + str(strings_hyp[i]))
+        bleu_score = nltk.translate.bleu_score.corpus_bleu(strings_ref, strings_hyp)
+        print("** Bleu score (corpus): " + str(bleu_score))
 
-            bleu_score = nltk.translate.bleu_score.sentence_bleu(strings_ref[i], strings_hyp[i])
-            cumulative_bleu_score += bleu_score
-            scored = scored + 1
+        show_nist(reference_file, hypotesis_file)
 
-    average_blue = cumulative_bleu_score/len(strings_ref)
-    print("** Bleu score (average): {0} (sentences {1})".format(str(average_blue), scored))
-    bleu_score = nltk.translate.bleu_score.corpus_bleu(strings_ref, strings_hyp)
-    print("** Bleu score (corpus): " + str(bleu_score))
+
+def show_nist(reference_file, hypotesis_file):
+    cumulative_bleu_score = 0
+    with open(reference_file, 'r') as tf_ref, open(hypotesis_file, 'r') as tf_hyp:
+        lines_ref = tf_ref.read().splitlines()
+        lines_hyp = tf_hyp.read().splitlines()
+#        lines_ref = lines_ref[0:500]
+#        lines_hyp = lines_hyp[0:500]
+
+        if len(lines_ref) != len(lines_hyp):
+            print("Different number of lines in files")
+            return
+
+        # Str -> to tokens
+        strings_ref = []
+        for i in range(0, len(lines_ref)):
+            strings_ref.append([(lines_ref[i].split())])# Double list
+
+        strings_hyp = []
+        for i in range(0, len(lines_hyp)):
+            strings_hyp.append(lines_hyp[i].split())
+
+        bleu_score = nist_score.corpus_nist(strings_ref, strings_hyp)
+        print("** NIST score (corpus): " + str(bleu_score))
 
     
 def main():
